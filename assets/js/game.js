ed5c90ba9c4984a144050c267908fa048c6ddbc6
danielle-gan <danielle.gan@uconn.edu>
2021-12-07 16:47:40 -0500

    create fight function and console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
